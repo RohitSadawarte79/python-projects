6defd26632676bab29dc5a72340b8df306a2c836
rohitsadawarte79 <rohitsadawarte777@gmail.com>
2025-12-24 20:48:05 +0530

    added login functionality

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -19,17 +19,17 @@ def initialize_cipher(key):
 
 def register(user_name , master_password):
     #Encrypt the master password before storing it
-    hashed_master_pass = hash_password(master_password)
-    user_data = {'user_name': user_name, 'master_password': hashed_master_pass}
-    file_name = 'user_data.json'
+    hashed_master_pass = hash_password(master_password) # the SHA-256 algo will convert this into hash hex string 
+    user_data = {'user_name': user_name, 'master_password': hashed_master_pass} # then we store the user data in dictionary
+    file_name = 'user_data.json' #this is the file name where the user's data will go and be saved 
 
-    if os.path.exists(file_name):
+    if os.path.exists(file_name): # if the file already exists then overwrite it
         with open(file_name, 'w') as file:
-            json.dump(user_data, file) 
+            json.dump(user_data, file) # put the user_data into the json file 
             print("\n[+] Registration complete!!\n")
     else:
-        with open(file_name, 'x') as file:
-            json.dump(user_data, file)
+        with open(file_name, 'x') as file: # if does not exists then create one 
+            json.dump(user_data, file) #put the user_data into the json file
             print("\n[+] Registration complete!!\n")
 
 #Function to encrypt a password.
@@ -40,9 +40,23 @@ def encrypt_password(cipher, password):
 def decrypt_password(cipher, encrypted_password):
     return cipher.decrypt(encrypted_password.encode()).decode()
 
-def register()
+def login(username , entered_password):
+    try:
+        with open('user_data.json', 'r') as file: #reading file user_data.json
+            user_data = json.load(file)           #loading file by json.load into user_data variable
+        
+        stored_password_hash = user_data.get('master_password')
+        entered_password_hash = hash_password(entered_password)
 
-database = {} #account : password
+        if stored_password_hash == entered_password_hash and username == user_data.get('user_name'):
+            print("\n[+] Login Successful...\n")
+        else:
+            print("\n[-] Invalid login credentials. Please use the credentials you used to register.\n")
+            sys.exit()
+    except Exception:
+        print("\n[-] You have not registered. Please do that. \n")
+        sys.exit()
+
 
 def view():
     pass
